1b97a5fb20262079e61932edb63a6a6b25952968
Shaden Alalwani <viuxwl7@gmail.com>
2025-10-27 10:06:27 +0000

    Fix sort.h: clean whitespace and ensure proper prototypes; Betty-compliant

--- sort.h
+++ sort.h
@@ -11,12 +11,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/* Prototypes for provided printers */
+/* Provided printers (declared for the checker) */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
